e6bbec124e99d8620efa0b410c923d458641a983
rprell <rileyrprell@gmail.com>
2022-11-23 09:25:30 -0500

    Added the fields for Apple and started the describeSelf in super class Phone

--- src/Phone.java
+++ src/Phone.java
@@ -1,19 +1,24 @@
 public class Phone {
     int storage;
     int screenSize;
+    int price;
     String processor;
-    int Price;
     String chargerType;
+    String phoneType;
 
     public Phone() {
 
     }
 
-    public Phone(int storage, int screenSize, String processor, int Price, String chargerType) {
+    public Phone(String phoneType, int storage, int screenSize, String processor, int price, String chargerType) {
         this.storage = storage;
         this.screenSize = screenSize;
         this.processor = processor;
-        this.Price = Price;
+        this.price = price;
         this.chargerType = chargerType;
+        this.phoneType = phoneType;
+    }
+    void describeSelf() {
+        System.out.println("The" + phoneType + " has" + storage + "GB of storage, a screen size of" + screenSize + "in, a" + processor + ", it uses the" + chargerType);
     }
 }
