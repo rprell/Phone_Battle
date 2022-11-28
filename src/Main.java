3229029ffcad523e67000c674dabbcba866f23fd
rprell <rileyrprell@gmail.com>
2022-11-28 14:54:47 -0500

    Staretd work on main, added the arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
 
 public class Main {
 
+    public static void main(String[] args) {
 
+        ArrayList<Phone> phones = new ArrayList<>();
+        Phone iphone = new Phone("apple", 128, 6, "a new one", 1000, "lightning");
+
+    }
+        Phone.describeSelf();
 
 }
