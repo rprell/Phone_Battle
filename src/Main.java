16078bca2d4678ae7947267553880db8afa2169c
rprell <rileyrprell@gmail.com>
2022-11-28 15:26:40 -0500

    Added samsung class

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,10 @@ public class Main {
 
         ArrayList<Phone> phones = new ArrayList<>();
         Phone iphone = new Phone("iPhone 13", 128, 6.1f, "Apple A15 Bionic", 1000, "lightning");
+        Phone samsung = new Phone("Galaxy S22", 128, 6.1f, "2.7 GHz octa-core", 599, "wireless");
 
         iphone.describeSelf();
+        samsung.describeSelf();
     }
 
 
