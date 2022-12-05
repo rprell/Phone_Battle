a45280437ea87fa2abaa4d8829006b0328ef37a6
Owenlin <owenlin1604@gmail.com>
2022-12-05 14:25:58 -0500

    Constructor and fields for samsung

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 import java.util.ArrayList;
 
+
 public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Phone> phones = new ArrayList<>();
-        Phone iphone = new Apple("iPhone 13", 128, 6.1f, "Apple A15 Bionic", 1000, "lightning", "true", "true", "true", "true");
-        Phone samsung = new Phone("Galaxy S22", 128, 6.1f, "2.7 GHz octa-core", 599, "wireless");
+        new Apple ("iPhone 13", 128, 6.1f, "Apple A15 Bionic", 1000, "lightning", "true", "true", "true", "true");
+        new Samsung ("Galaxy S22", 128, 6.1f, "2.7 GHz octa-core", 599, "wireless", "No", "Yes", 8);
 
-        iphone.describeSelf();
-        samsung.describeSelf();
-
-        for(Phone phone: phones) {
+        for(Phone phone: Phone.getPhones()) {
             phone.describeSelf();
         }
     }
