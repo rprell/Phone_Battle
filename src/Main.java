2aebd64d4d47becccc0bbc3cb2e9d433f3335da6
Owenlin <owenlin1604@gmail.com>
2022-12-05 14:43:58 -0500

    Constructor and fields for samsung

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main {
         new Apple ("iPhone 13", 128, 6.1f, "Apple A15 Bionic", 1000, "lightning", "true", "true", "true", "true");
         new Samsung ("Galaxy S22", 128, 6.1f, "2.7 GHz octa-core", 599, "wireless", "No", "Yes", 8);
 
-
         for(Phone phone: Phone.getPhones()) {
         System.out.println(phone.toString());
         }
