2b97b07f6eb4cc460e56c1a879ea0aef0024464d
Owenlin <owenlin1604@gmail.com>
2022-12-09 12:55:35 -0500

    Constructor and fields for samsung

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,6 @@ public class Main {
         for(Phone phone: Phone.getPhones()) {
         System.out.println(phone.toString());
         }
-
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter username");
-
-        String userName = myObj.nextLine();  // Read user input
-        System.out.println("Username is: " + userName);  // Output user input
-
     }
 
 
